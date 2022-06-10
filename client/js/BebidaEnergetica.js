b1051689be0a69a981b2532255beb7f0e0155b93
sebafrugar <sebafrugar@gmail.com>
2022-06-09 20:59:28 -0400

    creacion de filtro y limpieza de codigo

--- client/js/BebidaEnergetica.js
+++ client/js/BebidaEnergetica.js
@@ -1,9 +1,11 @@
 const request = fetch("http://localhost:8000/api/prueba")
+let productostotal= [];
 
 fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();
     }).then((response) =>{
+        productostotal = response.productos
         const filtrar = response.productos.filter(filtro => filtro.category === 1);
         console.log(filtrar)
         document.getElementById("producto").innerHTML = response.productos.filter(filtro => filtro.category === 1).map(producto => `
@@ -21,8 +23,8 @@ fetch('http://localhost:8000/api/prueba')
     })
     .catch(error => console.log(error))
 
-let porNombre = () => {
-    fetch('http://localhost:8000/api/prueba')
+let porNombre = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -43,8 +45,8 @@ let porNombre = () => {
     })
 }
 
-let porPrecioMenorMayor = () => {
-    fetch('http://localhost:8000/api/prueba')
+let porPrecioMenorMayor = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -67,9 +69,8 @@ let porPrecioMenorMayor = () => {
     })
 }
 
-let porPrecioMayorMenor = () => {
-    console.log("boton funcionando");
-    fetch('http://localhost:8000/api/prueba')
+let porPrecioMayorMenor = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -92,9 +93,8 @@ let porPrecioMayorMenor = () => {
     })
 }
 
-let porStock = () => {
-    console.log("boton funcionando");
-    fetch('http://localhost:8000/api/prueba')
+let porStock = async() =>{
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -116,3 +116,29 @@ let porStock = () => {
         `)
     })
 }
+const formulario = document.getElementById("formulario")
+const boton = document.getElementById("search")
+
+const filtrar = async () => {
+    let busqueda = formulario.value.toUpperCase()
+    let resultado = []
+    productostotal.map(producto => {
+        if(producto.name.toUpperCase().includes(busqueda) === true){
+            resultado.push(producto)
+        }
+    })
+    document.getElementById("busqueda").innerHTML = resultado.map(producto => `
+            <div class="card d-inline-block m-2" style="width: 17rem ;">
+                <div class="card-header rounded text-center d-block">
+                    <img src="${producto.url_image}" alt="imagen producto" srcset="" style="width: 100px; height: 150px ;align-middle;">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${producto.name}</li>
+                    <li class="list-group-item">Valor : $ ${producto.price}</li>
+                    <li class="list-group-item">Unidades Restantes :  ${producto.discount}</li>
+                </ul>
+            </div>    
+        `)
+};
+
+boton.addEventListener("click", filtrar)
--- client/js/Vodka.js
+++ client/js/Vodka.js
@@ -1,4 +1,5 @@
 const request = fetch("http://localhost:8000/api/prueba")
+let productostotal= [];
 
 fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
@@ -21,8 +22,8 @@ fetch('http://localhost:8000/api/prueba')
     })
     .catch(error => console.log(error))
 
-let porNombre = () => {
-    fetch('http://localhost:8000/api/prueba')
+let porNombre = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -43,8 +44,8 @@ let porNombre = () => {
     })
 }
 
-let porPrecioMenorMayor = () => {
-    fetch('http://localhost:8000/api/prueba')
+let porPrecioMenorMayor = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -67,9 +68,8 @@ let porPrecioMenorMayor = () => {
     })
 }
 
-let porPrecioMayorMenor = () => {
-    console.log("boton funcionando");
-    fetch('http://localhost:8000/api/prueba')
+let porPrecioMayorMenor = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -92,9 +92,8 @@ let porPrecioMayorMenor = () => {
     })
 }
 
-let porStock = () => {
-    console.log("boton funcionando");
-    fetch('http://localhost:8000/api/prueba')
+let porStock = async() => {
+    await fetch('http://localhost:8000/api/prueba')
     .then((response) =>{
         return response.json();})
     .then((response) =>{
@@ -116,4 +115,29 @@ let porStock = () => {
         `)
     })
 }
-    
+const formulario = document.getElementById("formulario")
+const boton = document.getElementById("search")
+
+const filtrar = async () => {
+    let busqueda = formulario.value.toUpperCase()
+    let resultado = []
+    productostotal.map(producto => {
+        if(producto.name.toUpperCase().includes(busqueda) === true){
+            resultado.push(producto)
+        }
+    })
+    document.getElementById("busqueda").innerHTML = resultado.map(producto => `
+            <div class="card d-inline-block m-2" style="width: 17rem ;">
+                <div class="card-header rounded text-center d-block">
+                    <img src="${producto.url_image}" alt="imagen producto" srcset="" style="width: 100px; height: 150px ;align-middle;">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">${producto.name}</li>
+                    <li class="list-group-item">Valor : $ ${producto.price}</li>
+                    <li class="list-group-item">Unidades Restantes :  ${producto.discount}</li>
+                </ul>
+            </div>    
+        `)
+};
+
+boton.addEventListener("click", filtrar) 
